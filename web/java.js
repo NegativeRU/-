d2802d05694a85f3ac9d47a41ec0139c4cb0c64c
NegativeRU <alpfags@gmail.com>
2021-03-20 09:54:31 +0500

    Update java.js

--- web/java.js
+++ web/java.js
@@ -1,3 +1,12 @@
-$('.message a').click(function(){
-   $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
-});
+function myFunction1() {
+var txt;
+if (confirm("Отправить данные?")) {
+txt = "Ваши данные отправлены!";
+} else {
+txt = "Данные не отправлены!";
+}
+document.getElementById("index").innerHTML = txt;
+}
+function myFunction2(){
+alert('Ваши данные отправлены!');
+}
